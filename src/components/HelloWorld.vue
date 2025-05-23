ca35bf685d81620dbb3a0affe180d8931bf2341c
Rifasetriyanda <Rifasetriyanda@student.uir.ac.id>
2025-05-23 18:06:59 +0700

    Perubahan Pada HelloWord dengan Menambahkan Biodata Diri

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -13,10 +13,10 @@ const count = ref(0)
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+    <p>BIODATA DIRI</p>
+    <p>Nama : Rifasetriyanda Rahmah</p>
+    <p>Kelas : 4D</p>
+    <p>Npm : 233510842</p>
   </div>
 
   <p>
